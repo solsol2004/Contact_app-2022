aa650c3face75cdedf998cc3362e3546ebef97ef
Stella <dpthf24@gmail.com>
2022-02-20 20:59:33 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-  home: Center( 
-    child: Image.asset('icon.jpg')
-  )
-);
+      home: Scaffold (
+        appBar: AppBar(title: Text('앱 제목')),
+        body: Text('안뇽'),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
+            ),
+          )
+        ),
+      )
+    );
   }
 }
