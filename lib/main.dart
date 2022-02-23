efa098158793771f59a631fcdd8244fff5793350
Stella <dpthf24@gmail.com>
2022-02-23 22:52:45 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home:MyApp()
+    )
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var name=['예쏠', '뭉치', '빠빠'];
+  var like=[0,2,3];
+  var valueText= 'hi';
+
+  @override
+  Widget build(BuildContext context)=> Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child:Text('+'), 
+          onPressed: (){
+            showDialog(context: context, builder: (context){
+              return DesignUI();
+              },
+            );
+          },
+        ),
+        appBar: AppBar(title: Text('contact')),
+        body: ListView.builder(
+          itemCount:3,
+          itemBuilder: (c,i){
+            return  ListTile(
+              leading: Icon(Icons.person_pin), 
+              title: Text(name[i]),
+             );
+            }
+          ),
+        bottomNavigationBar: BottomBar(),
+    );
+}
+
+class DesignUI extends StatelessWidget {
+  const DesignUI({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold (
-        appBar: AppBar(
-          backgroundColor: Colors.white ,
-          toolbarHeight: 50,
-            title:Text('회천3동',
-              style: TextStyle(
-                color:Colors.black, fontSize: 17, fontWeight: FontWeight.w600)
-              ),  
-              actions: 
-              [Icon(Icons.keyboard_arrow_down, color:Colors.black), Icon(Icons.search,color: Colors.black), Icon(Icons.menu, color: Colors.black), Icon(Icons.notifications_none, color: Colors.black)]
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 200,
+        child: Column(
+          children: [
+            TextField(),
+            TextButton(
+              onPressed: (){},
+              child: Text('완료')),
+            TextButton(
+              onPressed: (){Navigator.pop(context);},
+              child: Text('취소'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar( 
+      child: SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page)
+              ],
             ),
-        body: Container( 
-          width: double.infinity,
-          height: 120,
-          padding: EdgeInsets.fromLTRB(30, 12, 20, 12),
-          decoration: BoxDecoration(color: Colors.white,  ),
-          child: Row(
-            children: [
-              ClipRRect(
-                 borderRadius: BorderRadius.circular(8.0),
-                child:Image.asset('icon.jpg'), 
-              ),
-              Expanded( child: Container(
-                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('귀문관 키트 판매합니다 선제 3만원', style: TextStyle(fontWeight: FontWeight.w400),),
-                      Text('회천3동 끌올 2분전', style: TextStyle(color: Colors.grey, fontSize: 12),),
-                      Text('222,000원', style: TextStyle(fontWeight: FontWeight.w600),),
-                      SizedBox(
-                        height: 20,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Icon(Icons.favorite_border_rounded, size: 16, color: Colors.grey,), Text('4', style: TextStyle(color: Colors.grey),),
-                          ],
-                        ),
-                      ),
-                    ],
-                ),
-              ),
-              )
-            ],
           ),
-        )      
-      )
     );
   }
 }
